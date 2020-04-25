59e6a153817f3b349cee0d32b2c12c88bdef905a
Renzo Chávez <rlchavezc@outlook.com>
2020-04-24 23:59:20 -0500

    - Added all tags to search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { data_link_covid } from './data-covid';
+import { data_link_covid, DataLinkCovid } from './data-covid';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,26 @@ import { data_link_covid } from './data-covid';
 })
 export class AppComponent implements OnInit {
   title = 'portalcovidperu';
-  data_links: any[];
+  data_links: DataLinkCovid[];
   searchText: string;
   today: number;
+  allTags: string[] = [];
+  showAllTags = true;
+
   AppComponent() { }
 
   ngOnInit(): void {
     this.data_links = data_link_covid;
     this.today = Date.now();
+    this.data_links.forEach( link => {
+      link.tags.forEach( tag => {
+        if(!this.allTags.find( finalTag => finalTag == tag )) this.allTags.push(tag);
+      })
+    });
+    this.allTags.sort();
+  }
+
+  toogleViewTags(){
+    this.showAllTags = !this.showAllTags;
   }
 }
